4661db2b3dd9e80f80fe2212f0161dac63679654
attoumbre <juvenalak@gmail.com>
2022-03-13 19:23:06 +0100

    modif de signin

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,8 +1,8 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-const AUTH_API = 'http://localhost:8080/api/auth/';
+import { Observable, Subscriber } from 'rxjs';
+const AUTH_API = 'http://localhost:8080/';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -11,11 +11,22 @@ const httpOptions = {
 })
 export class AuthService {
   constructor(private http: HttpClient) { }
-  login(username: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
-      username,
-      password
-    }, httpOptions);
+  login(uName: string, uMail: string) {
+
+    const loginData = {
+      username: uName,
+      email : uMail
+    }
+    return new Observable<boolean>((observer)=>{
+      this.http.post(AUTH_API + 'signin', loginData).subscribe(result =>{
+        observer.next(true);
+        observer.complete();
+      }, error =>{
+        observer.error(false);
+        observer.complete();
+      });
+   } );
+      
   }
   register(username: string, email: string, password: string): Observable<any> {
     return this.http.post(AUTH_API + 'signup', {
