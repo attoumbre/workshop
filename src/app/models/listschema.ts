50bd6016a56285d37f723543c992d5f87143d013
attoumbre <juvenalak@gmail.com>
2022-04-27 09:34:30 +0200

    modif format du model

--- src/app/models/listschema.ts
+++ src/app/models/listschema.ts
@@ -4,4 +4,5 @@ export interface ListSchema {
     id: string;
     name: string;
     tasks: TaskSchema[];
+    //idTableau: number;
 }
--- src/app/models/taskschema.ts
+++ src/app/models/taskschema.ts
@@ -2,6 +2,8 @@ export interface TaskSchema {
   id: string;
   description: string;
   date: Date | string;
+  //temps: any;
   priority: string;
+  //userId: number;
   listId?: string;
 }
